9daa887db4b2a7cf994cd18cbd0a9a8b096faa77
Matheus Alves <mathalves@proton.me>
2022-11-15 23:28:42 -0300

    added views

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('customers/', views.customers, name='customers'),
+    path('customers/new/', views.customer_new, name='customer_new'),
+    path('customers/<int:customer_id>/',
+         views.customer_details, name='customer_details'),
+    path('services/', views.services, name='services'),
+    path('services/new/', views.service_new, name='service_new'),
     path('services/<int:service_id>/',
          views.service_details, name='service_details'),
 ]
--- app/views.py
+++ app/views.py
@@ -10,7 +10,34 @@ def index(request):
     return render(request, 'app/index.html', {'services': today_services})
 
 
+def customers(request):
+    return 'foo'
+
+
+def customer_new(request):
+    return 'foo'
+
+
+def customer_details(request):
+    return 'foo'
+
+
+def services(request):
+    return 'foo'
+
+
 def service_details(request, service_id):
     service = get_object_or_404(Service, pk=service_id)
 
     return render(request, 'app/service-details.html', {'service': service})
+
+
+def service_new(request):
+    customers, query = [], ''
+
+    if 's' in request.GET:
+        query = request.GET['s']
+        customers = Customer.objects.filter(
+            name__contains=query).order_by('-created_at')
+
+    return render(request, 'app/service-new.html', {'customers': customers, 'query': query})
